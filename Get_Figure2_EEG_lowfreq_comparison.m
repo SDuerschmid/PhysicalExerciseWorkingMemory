b5974d6c79bc7b73556f98df7e486ba89a06211f
XinyunChe <122985788+FreyaChe@users.noreply.github.com>
2025-07-02 11:32:14 +0800

    Update Get_Figure2_EEG_lowfreq_comparison.m
    change ylim

--- Get_Figure2_EEG_lowfreq_comparison.m
+++ Get_Figure2_EEG_lowfreq_comparison.m
@@ -13,13 +13,14 @@ Ep                                          = std( P,0,2 )./sqrt( 21 );
 
 figure; hold on
 x                                           = [1 2 2 1 1];
-y                                           = [-10 -10 30  30 -10];
+y                                           = [-15 -15 35 35 -15];
 v                                           = [x' y'];
 patch( 'Faces',[1:5],'vertices',v,'facecolor',[.7 .7 .7],'edgecolor','w')
 shadedErrorBar( time,mean( P,2 ),Ep,'r' );
 shadedErrorBar( time,mean( R,2 ),Er,'b' )
-myaxis( 'time','standardized amplitude' )
+myaxis( 'time [sec]','standardized amplitude' )
 xlim([-.5 2.5]);
+ylim([-15 35])
 pbaspect([1 1 1])
 
 
